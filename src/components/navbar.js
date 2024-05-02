7a10c0acb772ff11e4dccfcc0e6ccc1ee3401b5a
mboranian <77823899+mboranian@users.noreply.github.com>
2024-05-02 10:57:45 -0400

    some stuff now

---
+++ src/components/navbar.js
@@ -0,0 +1,31 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function BasicExample() {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary" bg='dark' data-bs-theme='dark' fixed='top'>
+      <Container>
+        <Navbar.Brand href="#home">Matthew Boranian</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">About</Nav.Link>
+            <NavDropdown title="Case Studies" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Responsive Redesign</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Development</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">Third Thing</NavDropdown.Item>
+              {/* <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">
+                Separated link
+              </NavDropdown.Item> */}
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default BasicExample;
